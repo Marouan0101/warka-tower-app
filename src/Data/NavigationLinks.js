dcd4b58d54ac84f3e31effa997e4931a9952d28e
Marouan El Yattafti <marouanely@gmail.com>
2022-05-01 15:25:44 +0200

    dfgsdfg

--- src/Data/NavigationLinks.js
+++ src/Data/NavigationLinks.js
@@ -1,9 +1,9 @@
 const navigationLinks = [
   { id: 1, title: 'Home', url: '/' },
   { id: 2, title: 'Warka Index', url: '/WarkaIndex' },
-  { id: 3, title: 'Our Goal', url: 'OurGoal' },
+  { id: 3, title: 'Our Goal', url: '/OurGoal' },
   { id: 4, title: 'Donate', url: '/Donate' },
-  { id: 5, title: 'About Us', url: 'About Us' },
+  { id: 5, title: 'About Us', url: '/AboutUs' },
 ];
 
 export default navigationLinks;
--- src/pages/BadRequest.js
+++ src/pages/BadRequest.js
@@ -3,10 +3,19 @@ import Navbar from '../components/Navbar';
 
 const BadRequest = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1 className='text-5xl text-center font-bold'>Nothing here yet...</h1>
-    </div>
+      <div className='container'>
+        <div className='text-center mt-20 bg-gradient-to-r from-secondary to-secondary-light bg-clip-text text-transparent'>
+          <h1 className='text-3xl sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-extrabold mb-10'>
+            This Page Is Under Construction
+          </h1>
+          <h2 className='text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold'>
+            New Content Coming Soon
+          </h2>
+        </div>
+      </div>
+    </>
   );
 };
 
